7cf8b0ca20cce10b47fe8dd5ed75a9413be85fac
Evan001414 <64682804+Evan001414@users.noreply.github.com>
2020-12-18 19:51:33 +0700

    Update Weather.js

--- weatherrea/src/components/Weather.js
+++ weatherrea/src/components/Weather.js
@@ -25,6 +25,18 @@ const Weather = props => (
 	 { 
 	 	props.error && <p className="weather__error">{ props.error }</p>  
 	 }
+	{
+		props.minTemp &&
+		<p className="weather__key">
+			Min. Temp :
+			<span className="weather__value">
+			 {props.minTemp} 
+			</span>
+		</p>
+	}
+	{
+		props.maxTemp &&<p className="weather__key">Max. Temp :<span className="weather__value">{props.maxTemp}</span></p>
+	}
 	</div>
 );
 
