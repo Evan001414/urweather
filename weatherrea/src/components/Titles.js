a345f3a71bbc82c715c7dcdba73794fa4b52ba63
violyy <76200560+violyy@users.noreply.github.com>
2020-12-18 14:42:58 +0700

    Update Titles.js

--- weatherrea/src/components/Titles.js
+++ weatherrea/src/components/Titles.js
@@ -3,7 +3,7 @@ import React from "react";
 const Titles = () => (
 	<div>
 		<h1 className="title-container__title">Ur Weather</h1>
-		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+		<h3 className="title-container__subtitle">Helping you find temperture, humidity and more! through your location~</h3>
 	</div>
 );
 
